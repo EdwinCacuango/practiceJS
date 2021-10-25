97ac3b734bfe3473a415663f0964487d83a23a27
Edwin Cacuango <edwin-fernando@hotmail.es>
2021-10-25 12:37:45 -0500

    correciones

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-const dataFromApi=async()=>{
-    try{
-        const response=await fetch(
-            "https://jsonplaceholder.typicode.com/todds/1");
-        const jsonResponse=await response.json();
+const dataFromApi = async () => {
+  try {
+    const response = await fetch(
+      "https://jsonplaceholder.typicode.com/todos/1"
+    );
+    const jsonResponse = await response.json();
+    return jsonResponse
+  } catch (error) {
+    console.log("error=>", error);
+  }
+};
 
-        return jsonResponse;
-    }catch (error){
-        console.log("error=>", error)
-    }
-}
-
-console.log(dataFromApi)
+console.log(dataFromApi().then((response)=>console.log(response)))
